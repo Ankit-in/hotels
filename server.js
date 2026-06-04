d7e8f853ae14ea2dc4eb4be4d2732e65a56a34aa
Ankit Kumar Singh <ankitsingh7319@gmail.com>
2026-06-04 16:59:01 +0530

    lastest disable code

--- server.js
+++ server.js
@@ -91,8 +91,8 @@ const app = express();
 const db = require('./db');
 require('dotenv').config();
 
-const cors = require('cors');
-app.use(cors());
+// const cors = require('cors');
+// app.use(cors());
 
 const PORT = process.env.PORT || 3000;
 //body-parser is use to parser and extract the data incoming http request body data
