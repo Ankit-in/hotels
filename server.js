105a8a49bd92737799a1a91089a038b15574c3fa
Ankit Kumar Singh <ankitsingh7319@gmail.com>
2026-06-04 14:04:33 +0530

    hosting at render.com

--- server.js
+++ server.js
@@ -110,5 +110,5 @@ app.use('/person', personRoutes);
 app.use('/menuItem', menuItemRoutes);
 
 app.listen(PORT, () => {
-  console.log('Server is running on http://localhost:3000')
+  console.log(`Server is running on port ${PORT}`);
 })
